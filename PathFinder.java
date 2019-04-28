85a95a206d15043e137a76e33145bc478ce3c513
Stu <stu.ellerbusch@gmail.com>
2019-04-28 19:50:13 -0400

    Threads

--- PathFinder.java
+++ PathFinder.java
@@ -1,36 +1,46 @@
 import java.util.ArrayList;
 import java.util.Map;
 
-public class PathFinder {
+public class PathFinder implements Runnable {
 	private Map<String, Location> locations;
 	private ArrayList<Path> paths;
+	private Thread me;
+	private String start;
+	private String next;
 
-	public PathFinder(Map<String, Location> locs) {
+	public PathFinder(String start, String next, Map<String, Location> locs) {
+		this.start = start;
+		this.next = next;
 		this.locations = locs;
 		this.paths = new ArrayList<Path>();
 	}
 
-	public void outputPaths() {
+	public void start() {
+		System.out.println("Thread started");
+		if (me == null) {
+			me = new Thread(this, next);
+			me.start();
+		}
+	}
+
+	@Override
+	public void run() {
 		int count = this.locations.size();
 
 		String[] labels = new String[count];
 
 		this.locations.keySet().toArray(labels);
 
-		//for (int i = 0; i < labels.length; i++) {
-			ArrayList<String> x = new ArrayList<String>();
+		ArrayList<String> x = new ArrayList<String>();
 
-			for (int j = 1; j < labels.length; j++) {
-				//if (i != j) {
-					x.add(labels[j]);
-				//}
-			}
+		for (int j = 1; j < labels.length; j++) {
+			x.add(labels[j]);
+		}
 
-			Path path = new Path(this.locations.get(labels[0]), this.locations, x);
-			
-			this.paths.add(path);
-			
-			path.run();
-		//}
+		Path path = new Path(this.locations.get(labels[0]), this.locations, x);
+
+		this.paths.add(path);
+
+		path.run();
 	}
 }
--- Program.java
+++ Program.java
@@ -1,8 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Program {
 
@@ -10,10 +14,33 @@ public class Program {
 		Map<String, Location> locations = loadLocations();
 
 		System.out.println("Locations Found=" + locations.size());
+
+		String[] labels = new String[locations.size()];
+
+		locations.keySet().toArray(labels);
+
+		ExecutorService es = Executors.newCachedThreadPool();
+
+		ArrayList<PathFinder> finders = new ArrayList<PathFinder>();
+
+		for (int i = 1; i < labels.length; i++) {
+			PathFinder finder = new PathFinder(labels[0], labels[1], locations);
+
+			finders.add(finder);
+
+			es.execute(finder);
+		}
+
+		es.shutdown();
+
+		try {
+			boolean finished = es.awaitTermination(10, TimeUnit.MINUTES);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		PathFinder finder = new PathFinder(locations);
-		
-		finder.outputPaths();
+		//	find "BEST" from PathFinders
 	}
 
 	public static Map<String, Location> loadLocations() {
